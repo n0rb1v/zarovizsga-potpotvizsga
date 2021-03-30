ae906a7208901fcbb64a30fa36031421bbd96a41
n0rb1v <n0rb1v@gmail.com>
2021-03-30 14:10:07 +0200

    1

--- src/main/java/hu/nive/ujratervezes/zarovizsga/uniquefinder/UniqueFinder.java
+++ src/main/java/hu/nive/ujratervezes/zarovizsga/uniquefinder/UniqueFinder.java
@@ -1,4 +1,17 @@
 package hu.nive.ujratervezes.zarovizsga.uniquefinder;
 
+import java.util.*;
+
 public class UniqueFinder {
+
+    public List<Character> uniqueChars(String s) {
+        if (s == null) {
+            throw new IllegalArgumentException("parameter error");
+        }
+        Set<Character> result = new LinkedHashSet<>();
+        for (int i = 0; i < s.length(); i++) {
+            result.add(s.charAt(i));
+        }
+        return new ArrayList<>(result);
+    }
 }
